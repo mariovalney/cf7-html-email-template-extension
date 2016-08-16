8c426b59aed1e279cdaec7cd62a69271afa35a49
Mário Valney <mariovalney@gmail.com>
2016-08-15 22:03:42 -0300

    Initial Commit: Structure and Check for CF7 Active/Installed

---
+++ README.txt
@@ -0,0 +1,48 @@
+=== Plugin Name ===
+Contributors: mariovalney
+Donate link: http://mariovalney.com
+Tags: comments, spam
+Requires at least: 3.0.1
+Tested up to: 4.5
+Stable tag: 4.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Improve your Contact Form 7 emails with a HTML Template.
+
+== Description ==
+
+[Contact Form 7 (CF7)](https://wordpress.org/plugins/contact-form-7/ "Install it first, of course") is a awesome plugin used by 1+ million WordPress websites.
+
+But even it allow users to use HTML instead of just Plain Text email it's not trivial to users who aren't developers create something really pro.
+
+However, time to leave all your worries behind! As a recently launched "Contact Form 7 - HTML Mail Template Extension" will allow you, WordPress developers or not, to use a simply but beautiful HTML Template in your CF7 so users will get better emails than just amount of text. 
+
+= Configurations =
+
+Easily and quickly! No configurations required to start, but you can change a lot of stuff like colors, header image and width (by now... new versions will come).
+
+= Email Template =
+
+The plugin applies a HTML template to email sent from your CF7. The Template is optimized to show your HTML emails perfectly in the most popular email browsers including mobile ones.
+
+== Installation ==
+
+`Install [Contact Form 7](https://wordpress.org/plugins/contact-form-7/)`
+
+1. Upload `contact-form-7-html-mail-template-extension.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Done!
+
+== Frequently Asked Questions ==
+
+= Does it works for emails sent out of CF7? =
+
+Snopes. The intention here is to improve CF7 functionality.
+We'll create another version to all WordPress emails or add an option to extend this plugin, but not in this version.
+
+== Changelog ==
+
+= 1.0 =
+* It's alive!
+* Basic use and configurations.
---
+++ cf7-html-email-template-extension.php
@@ -0,0 +1,276 @@
+<?php
+
+/**
+ *
+ * @package           Cf7_Html_Email_Template_Extension
+ * @since             1.0.0
+ *
+ * Plugin Name:       Contact Form 7 - HTML Email Template Extension
+ * Plugin URI:        http://projetos.mariovalney.com/cf7-html-email-template-extension
+ * Description:       Improve your Contact Form 7 emails with a HTML Template.
+ * Version:           1.0.0
+ * Author:            Mário Valney
+ * Author URI:        http://mariovalney.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       cf7-html-email-template-extension
+ * Domain Path:       /languages
+ *
+ */
+
+// If this file is called directly, abort.
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+if ( ! function_exists( 'you_shall_not_pass' ) ) {
+	function you_shall_not_pass() {
+		defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+	}
+}
+
+if ( ! class_exists( 'Cf7_Html_Email_Template_Extension' ) ) {
+
+	class Cf7_Html_Email_Template_Extension {
+
+		/**
+		 * The unique internal identifier of this plugin to avoid overwritten class. 
+		 * Discussed with @gugaalves and @leobaiano in WordCamp Fortaleza 2016...
+		 *
+		 * @since    1.0.0
+		 * @access   public
+		 * @var      string    $class_tag	The string used to uniquely identify this class.
+		 */
+		public $class_tag;
+
+		/**
+		 * The unique identifier of this plugin.
+		 *
+		 * @since    1.0.0
+		 * @access   protected
+		 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+		 */
+		protected $plugin_name;
+
+		/**
+		 * The current version of the plugin.
+		 *
+		 * @since    1.0.0
+		 * @access   protected
+		 * @var      string    $version    The current version of the plugin.
+		 */
+		protected $version;
+
+		/**
+		 * The array of actions registered with WordPress.
+		 *
+		 * @since    1.0.0
+		 * @access   protected
+		 * @var      array    $actions    The actions registered with WordPress to fire when the plugin loads.
+		 */
+		protected $actions;
+
+		/**
+		 * The array of filters registered with WordPress.
+		 *
+		 * @since    1.0.0
+		 * @access   protected
+		 * @var      array    $filters    The filters registered with WordPress to fire when the plugin loads.
+		 */
+		protected $filters;
+
+		/**
+		 * Define the core functionality of the plugin.
+		 *
+		 * @since    1.0.0
+		 */
+		public function __construct() {
+
+			$this->plugin_name = CF7HETE_TEXTDOMAIN;
+			$this->version = CF7HETE_VERSION;
+			$this->class_tag = CF7HETE_TAG;
+
+			$this->actions = $this->filters = array();
+
+			$this->define_hooks();
+
+		}
+
+		/**
+		 * The name of the plugin.
+		 *
+		 * @since     1.0.0
+		 * @return    string    The name of the plugin.
+		 */
+		public function get_plugin_name() {
+			return $this->plugin_name;
+		}
+
+		/**
+		 * The version number of the plugin.
+		 *
+		 * @since     1.0.0
+		 * @return    string    The version number of the plugin.
+		 */
+		public function get_version() {
+			return $this->version;
+		}
+
+		/**
+		 * Register all of the hooks related to the admin area functionality
+		 * of the plugin.
+		 *
+		 * @since    1.0.0
+		 * @access   private
+		 */
+		private function define_hooks() {
+
+			// Internationalization
+			$this->add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
+
+			// Admin Hooks
+			$this->add_action( 'admin_notices', array( $this, 'check_cf7_plugin' ) );
+
+			//$this->add_action( 'admin_enqueue_scripts', 'enqueue_styles' );
+			//$this->add_action( 'admin_enqueue_scripts', 'enqueue_scripts' );
+			//$this->add_action( 'admin_notices', 'admin_notices' );
+
+		}
+
+		/**
+		 * A utility function that is used to register the actions and hooks into a single
+		 * collection.
+		 *
+		 * @since    1.0.0
+		 * @access   private
+		 * @param    array		$hooks				The collection of hooks that is being registered (that is, actions or filters).
+		 * @param    string 	$hook 				The name of the WordPress filter that is being registered.
+		 * @param    string 	$callback 			The callback function or a array( $obj, 'method' ) to public method of a class.
+		 * @param    int		$priority 			The priority at which the function should be fired.
+		 * @param    int		$accepted_args 		The number of arguments that should be passed to the $callback.
+		 * @return   array 							The collection of actions and filters registered with WordPress.
+		 */
+		private function add_hook( $hooks, $hook, $callback, $priority, $accepted_args ) {
+
+			$hooks[] = array(
+				'hook'          => $hook,
+				'callback'      => $callback,
+				'priority'      => $priority,
+				'accepted_args' => $accepted_args
+			);
+
+			return $hooks;
+
+		}
+
+		/**
+		 * Add a new action to the collection to be registered with WordPress.
+		 *
+		 * @since    1.0.0
+		 * @param    string		$hook             The name of the WordPress action that is being registered.
+		 * @param    string		$callback         The callback function or a array( $obj, 'method' ) to public method of a class.
+		 * @param    int		$priority         Optional. he priority at which the function should be fired. Default is 10.
+		 * @param    int		$accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
+		 */
+		public function add_action( $hook, $callback, $priority = 10, $accepted_args = 1 ) {
+
+			$this->actions = $this->add_hook( $this->actions, $hook, $callback, $priority, $accepted_args );
+
+		}
+
+		/**
+		 * Add a new filter to the collection to be registered with WordPress.
+		 *
+		 * @since    1.0.0
+		 * @param    string		$hook             The name of the WordPress filter that is being registered.
+		 * @param    string		$callback         The callback function or a array( $obj, 'method' ) to public method of a class.
+		 * @param    int		$priority         Optional. he priority at which the function should be fired. Default is 10.
+		 * @param    int		$accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1
+		 */
+		public function add_filter( $hook, $callback, $priority = 10, $accepted_args = 1 ) {
+
+			$this->filters = $this->add_hook( $this->filters, $hook, $callback, $priority, $accepted_args );
+
+		}
+
+		/**
+		 * Define the locale for this plugin for internationalization.
+		 *
+		 *
+		 * @since    1.0.0
+		 * @access   private
+		 */
+		public function load_plugin_textdomain() {
+
+			load_plugin_textdomain( CF7HETE_TEXTDOMAIN, false, basename( dirname( __FILE__ ) ) . '/languages' );
+
+		}
+
+		/**
+		 * Check Contact Form 7 Plugin is active
+		 * It's a dependency in this version
+		 *
+		 * @since    1.0.0
+		 * @access   private
+		 */
+		public function check_cf7_plugin() {
+
+			if ( ! is_plugin_active( 'contact-form-7/wp-contact-form-7.php' ) ) {
+				echo '<div class="notice notice-error is-dismissible">';
+				echo '<p>' . sprintf( __( "You need to install/activate %s Contact Form 7%s plugin to enable %s HTML Email Template Extension %s ", CF7HETE_TEXTDOMAIN ), '<a href="http://contactform7.com/" target="_blank">', '</a>', '<strong>', '</strong>' );
+				
+
+				if ( file_exists( ABSPATH . PLUGINDIR . '/contact-form-7/wp-contact-form-7.php' ) ) {
+					$url = 'plugins.php';
+				} else {
+					$url = 'plugin-install.php?tab=search&s=Contact+form+7';
+				}
+
+				echo '. <a href="' . admin_url( $url ) . '">' . __( "Do it now?", CF7HETE_TEXTDOMAIN ) . '</a></p>';
+				echo '</div>';
+			}
+
+		}
+
+		/**
+		 * Run the plugin.
+		 *
+		 * @since    1.0.0
+		 */
+		public function run() {
+
+			// Running Filters
+			foreach ( $this->filters as $hook ) {
+				add_filter( $hook['hook'], $hook['callback'], $hook['priority'], $hook['accepted_args'] );
+			}
+
+			// Running Actions
+			foreach ( $this->actions as $hook ) {
+				add_action( $hook['hook'], $hook['callback'], $hook['priority'], $hook['accepted_args'] );
+			}
+
+		}
+
+	}
+}
+
+/**
+ * Making things happening
+ *
+ * @since    1.0.0
+ */
+function cf7hete_starts() {
+
+	define( 'CF7HETE_VERSION', '1.0.0' );
+	define( 'CF7HETE_TEXTDOMAIN', 'cf7-html-email-template-extension' );
+	define( 'CF7HETE_TAG', 'udioDCODC1OdCzv-Q4hGlLLxy0t3q6V' );
+
+	$plugin = new Cf7_Html_Email_Template_Extension();
+
+	if ( CF7HETE_TAG == $plugin->class_tag ) {
+		$plugin->run();
+	} else {
+		trigger_error( __( 'The Cf7_Html_Email_Template_Extension was overwritten...' ), E_USER_WARNING );
+	}
+
+}
+
+cf7hete_starts();
